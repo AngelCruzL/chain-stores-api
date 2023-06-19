f3ad61ba8b0c4efc8d1d6c25fe66e7b55078a93c
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-06-19 13:44:02 -0600

    Add functionality to create a new store location

--- src/store/dto/create-store.dto.ts
+++ src/store/dto/create-store.dto.ts
@@ -1,1 +1,24 @@
-export class CreateStoreDto {}
+import { IsNumber, IsString } from 'class-validator';
+
+export class CreateStoreDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  address: string;
+
+  @IsString()
+  phone: string;
+
+  @IsString()
+  description: string;
+
+  @IsString()
+  picture: string;
+
+  @IsNumber()
+  latitude: number;
+
+  @IsNumber()
+  longitude: number;
+}
--- src/store/store.service.ts
+++ src/store/store.service.ts
@@ -1,11 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { Store } from './entities/store.entity';
 import { CreateStoreDto } from './dto/create-store.dto';
 import { UpdateStoreDto } from './dto/update-store.dto';
 
 @Injectable()
 export class StoreService {
-  create(createStoreDto: CreateStoreDto) {
-    return 'This action adds a new store';
+  constructor(
+    @InjectModel(Store.name)
+    private readonly storeModel: Model<Store>,
+  ) {}
+
+  async create(createStoreDto: CreateStoreDto) {
+    try {
+      const store = await this.storeModel.create(createStoreDto);
+      return store;
+    } catch (error) {
+      if (error.code === 11000)
+        throw new BadRequestException(
+          `Store already exists ${JSON.stringify(error.keyValue)}`,
+        );
+
+      console.log(error);
+      throw new InternalServerErrorException(`Error creating store`);
+    }
   }
 
   findAll() {
